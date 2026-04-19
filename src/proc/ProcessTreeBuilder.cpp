2104f0a1717a9f781243bb466c9d76c69f6f672b
ArtemHome <afirulin@gmail.com>
2026-04-19 16:47:59 +0300

    Change build tree algorithm

--- src/proc/ProcessTreeBuilder.cpp
+++ src/proc/ProcessTreeBuilder.cpp
@@ -1,37 +1,30 @@
 #include "ProcessTreeBuilder.h"
-#include <iostream>
+
+#include <unordered_map>
 
 std::vector<std::shared_ptr<ProcessInfo>> ProcessTreeBuilder::BuildTree(std::vector<std::unique_ptr<ProcessInfo>> &processes)
 {
+    std::unordered_map<uint32_t, std::shared_ptr<ProcessInfo>> processesMap;
     std::vector<std::shared_ptr<ProcessInfo>> roots;
+
     for (auto &proc : processes)
     {
-        if (proc != nullptr && (proc->parentPid == 0 || proc->parentPid == 1))
+        if (proc != nullptr /* && (proc->parentPid == 0 || proc->parentPid == 1)*/)
         {
-            std::shared_ptr<ProcessInfo> sharedProc = std::move(proc);
-            roots.push_back(sharedProc);
+            uint32_t pid = proc->pid;
+            processesMap[pid] = std::move(proc);
         }
     }
 
-    for (auto &root : roots)
+    for (auto const &[pid, proc] : processesMap)
     {
-        AttachChildren(root, processes);
+        auto it = processesMap.find(proc->parentPid);
+
+        if (it != processesMap.end() && pid != proc->parentPid)
+            it->second->children.push_back(proc);
+        else
+            roots.push_back(proc);
     }
 
     return roots;
-}
-
-void ProcessTreeBuilder::AttachChildren(
-    std::shared_ptr<ProcessInfo> parent,
-    std::vector<std::unique_ptr<ProcessInfo>> &processes)
-{
-    for (auto &proc : processes)
-    {
-        if (proc != nullptr && proc->parentPid == parent->pid)
-        {
-            std::shared_ptr<ProcessInfo> sharedProc = std::move(proc);
-            parent->children.push_back(sharedProc);
-            AttachChildren(sharedProc, processes);
-        }
-    }
 }
--- src/proc/ProcessTreeBuilder.h
+++ src/proc/ProcessTreeBuilder.h
@@ -9,8 +9,4 @@ class ProcessTreeBuilder
 {
 public:
     std::vector<std::shared_ptr<ProcessInfo>> BuildTree(std::vector<std::unique_ptr<ProcessInfo>> &processes);
-
-private:
-    void AttachChildren(std::shared_ptr<ProcessInfo> parent,
-                        std::vector<std::unique_ptr<ProcessInfo>> &processes);
 };
